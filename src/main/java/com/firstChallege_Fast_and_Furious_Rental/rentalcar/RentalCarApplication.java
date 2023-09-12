c7242f3793818962524a77ca3bb0840f539cf7f1
ThiagoMdO <128644651+ThiagoMdO@users.noreply.github.com>
2023-09-12 14:42:25 -0300

    default documents, project start

---
+++ src/main/java/com/firstChallege_Fast_and_Furious_Rental/rentalcar/RentalCarApplication.java
@@ -0,0 +1,13 @@
+package com.firstChallege_Fast_and_Furious_Rental.rentalcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalCarApplication.class, args);
+	}
+
+}
