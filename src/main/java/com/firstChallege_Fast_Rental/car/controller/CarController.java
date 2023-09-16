c59ebaeb2a83b85838f20f293b39547aaa36d4cb
ThiagoMdO <128644651+ThiagoMdO@users.noreply.github.com>
2023-09-16 11:14:15 -0300

    code improvement, not accepting null, empty and blank fields with StringUtils.hasText; return of updated httpStatus

--- src/main/java/com/firstChallege_Fast_Rental/car/controller/CarController.java
+++ src/main/java/com/firstChallege_Fast_Rental/car/controller/CarController.java
@@ -5,9 +5,11 @@ import com.firstChallege_Fast_Rental.car.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+
+import org.springframework.util.StringUtils;
+
 
 import java.util.List;
 import java.util.Objects;
@@ -22,24 +24,32 @@ public class CarController {
         return repository.findAll();
     }
     @GetMapping("/car/{id}")
-    public Car getAllCarsDetails(@PathVariable long id){
-        Optional<Car> car = repository.findById(id);
-        if(car.isEmpty()){
-            throw new RuntimeException("Car don't exist, with id: " + id);
+    public ResponseEntity<String> getAllCarsDetails(@PathVariable long id){
+        try{
+            Optional<Car> car = repository.findById(id);
+            if(car.get().getChassiId() != null){
+                String data = "Response 200: on success in your search " + car.get();
+                return ResponseEntity.ok(data);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
         }
-        return car.get();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Response 404: car doesn't found");
     }
 
     @PostMapping("/car")
-    public void createCar(@RequestBody Car car){
+    public ResponseEntity<String> createCar(@RequestBody Car car){
         try{
-            if(car.getBrand() == null || car.getBrand().isBlank()
-                    || car.getModel() == null || car.getModel().isBlank()
-                    || car.getColor() == null || car.getColor().isBlank()
-                    || car.getFabricationYear() == null || car.getFabricationYear().isBlank()
+            if(!StringUtils.hasText(car.getBrand())
+                    || !StringUtils.hasText(car.getModel())
+                    || !StringUtils.hasText(car.getColor())
+                    || !StringUtils.hasText(car.getFabricationYear())
             )
             {
-                throw new Exception("Please insert all of the fields in the fill");
+                String responseData = "Response 400: Bad Request: Please insert all of the fields in the form";
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+                //throw new IllegalArgumentException("Please insert all of the fields in the form");
+
             }else {
                 if(Objects.equals(car.getBrand(), "Ford")
                         || Objects.equals(car.getBrand(),"Chevrolet")
@@ -47,12 +57,17 @@ public class CarController {
                         || Objects.equals(car.getBrand(),"Volvo"))
                 {
                     repository.save(car);
+                    String responseData = "Response 201, success to create: " + car;
+                    return ResponseEntity.ok(responseData);
                 }else{
-                    throw new Exception("Erro 404, model not authorized for registration, the field brand needs to be one of this brands: Ford, Chevrolet, BMW or Volvo. Please try again");
+                    String responseData = "Response 404: Not found: Model not authorized for registration, the field brand needs to be one of this brands: Ford, Chevrolet, BMW or Volvo. Please try again";
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseData);
+                    //throw new IllegalArgumentException("Erro 404, model not authorized for registration, the field brand needs to be one of this brands: Ford, Chevrolet, BMW or Volvo. Please try again");
                 }
             }
-        } catch (Exception e){
+        } catch (IllegalArgumentException e){
             e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Error, restart the application");
         }
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,4 +5,4 @@ INSERT INTO car (chassi_id,model,brand,color,fabrication_year)
 VALUES (1002, 'New Fiesta','Ford','Gray','1999/2000');
 
 INSERT INTO car (chassi_id,model,brand,color,fabrication_year)
-VALUES (1003,'Fusion','Ford','Gray','2009/2010');
+VALUES (1003,'Fusion','Ford','Silver','2009/2010');
