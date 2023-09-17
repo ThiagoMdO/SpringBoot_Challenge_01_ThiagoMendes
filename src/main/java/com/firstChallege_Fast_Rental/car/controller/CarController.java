6b921b3b09dceca1c09b0f04f04e888939e37813
ThiagoMdO <128644651+ThiagoMdO@users.noreply.github.com>
2023-09-17 12:10:35 -0300

    creation of the service class, separate from the controller class

--- src/main/java/com/firstChallege_Fast_Rental/car/controller/CarController.java
+++ src/main/java/com/firstChallege_Fast_Rental/car/controller/CarController.java
@@ -1,73 +1,34 @@
 package com.firstChallege_Fast_Rental.car.controller;
 
 import com.firstChallege_Fast_Rental.car.bean.Car;
-import com.firstChallege_Fast_Rental.car.repository.CarRepository;
+import com.firstChallege_Fast_Rental.car.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
-
-import org.springframework.util.StringUtils;
-
-
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @RestController
 public class CarController {
     @Autowired
-    private CarRepository repository;
+    private CarService carService;
     @GetMapping("/car")
-    public List<Car> getAllCars(){
-        return repository.findAll();
+    public List<Car> getAllCars() {
+        return carService.getAllCars();
     }
     @GetMapping("/car/{id}")
-    public ResponseEntity<String> getAllCarsDetails(@PathVariable long id){
-        try{
-            Optional<Car> car = repository.findById(id);
-            if(car.get().getChassiId() != null){
-                String data = "Response 200: on success in your search " + car.get();
-                return ResponseEntity.ok(data);
-            }
-        } catch (Exception e){
-            e.printStackTrace();
+    public ResponseEntity<String> getAllCarsDetails(@PathVariable long id) {
+        Optional<Car> car = carService.getCarDetails(id);
+        if (car.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Response 404: car doesn't found");
         }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Response 404: car doesn't found");
     }
-
     @PostMapping("/car")
-    public ResponseEntity<String> createCar(@RequestBody Car car){
-        try{
-            if(!StringUtils.hasText(car.getBrand())
-                    || !StringUtils.hasText(car.getModel())
-                    || !StringUtils.hasText(car.getColor())
-                    || !StringUtils.hasText(car.getFabricationYear())
-            )
-            {
-                String responseData = "Response 400: Bad Request: Please insert all of the fields in the form";
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
-                //throw new IllegalArgumentException("Please insert all of the fields in the form");
-
-            }else {
-                if(Objects.equals(car.getBrand(), "Ford")
-                        || Objects.equals(car.getBrand(),"Chevrolet")
-                        || Objects.equals(car.getBrand(),"BMW")
-                        || Objects.equals(car.getBrand(),"Volvo"))
-                {
-                    repository.save(car);
-                    String responseData = "Response 201, success to create: " + car;
-                    return ResponseEntity.ok(responseData);
-                }else{
-                    String responseData = "Response 404: Not found: Model not authorized for registration, the field brand needs to be one of this brands: Ford, Chevrolet, BMW or Volvo. Please try again";
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseData);
-                    //throw new IllegalArgumentException("Erro 404, model not authorized for registration, the field brand needs to be one of this brands: Ford, Chevrolet, BMW or Volvo. Please try again");
-                }
-            }
-        } catch (IllegalArgumentException e){
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Error, restart the application");
-        }
+    public ResponseEntity<String> createCar(@RequestBody Car car) {
+        return carService.createCar(car);
     }
 }
---
+++ src/main/java/com/firstChallege_Fast_Rental/car/service/CarService.java
@@ -0,0 +1,56 @@
+package com.firstChallege_Fast_Rental.car.service;
+
+import com.firstChallege_Fast_Rental.car.bean.Car;
+import com.firstChallege_Fast_Rental.car.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class CarService {
+
+    @Autowired
+    private CarRepository repository;
+
+    public List<Car> getAllCars() {
+        return repository.findAll();
+    }
+
+    public Optional<Car> getCarDetails(long id) {
+        return repository.findById(id);
+    }
+
+    public ResponseEntity<String> createCar(Car car) {
+        try {
+            if (!StringUtils.hasText(car.getBrand())
+                    || !StringUtils.hasText(car.getModel())
+                    || !StringUtils.hasText(car.getColor())
+                    || !StringUtils.hasText(car.getFabricationYear())) {
+                throw new IllegalArgumentException("Please insert all of the fields in the form correctly");
+//                String responseData = "Response 422: Failure to validate data. Please insert all of the fields in the form correctly";
+//                return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(responseData);
+            } else {
+                if (Objects.equals(car.getBrand(), "Ford")
+                        || Objects.equals(car.getBrand(), "Chevrolet")
+                        || Objects.equals(car.getBrand(), "BMW")
+                        || Objects.equals(car.getBrand(), "Volvo")) {
+                    repository.save(car);
+                    return ResponseEntity.status(HttpStatus.CREATED).build();
+                } else {
+                    String responseData = "Response 404: Not found: Brand not authorized for registration, the field brand needs to be one of this brands: Ford, Chevrolet, BMW or Volvo. Please try again";
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseData);
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Response 406. Type of content not acceptable, restart the application");
+        }
+    }
+}
+
